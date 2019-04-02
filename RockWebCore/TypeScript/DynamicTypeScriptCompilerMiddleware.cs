fbf999db57fd9bbea2ebd2053e5330dc5ae75bac
Daniel Hazelbaker <daniel@blueboxmoon.com>
2019-04-02 16:28:43 -0700

    - Update dynamic TypeScript middleware to cache for 1 hour by default.

--- RockWebCore/TypeScript/DynamicTypeScriptCompilerMiddleware.cs
+++ RockWebCore/TypeScript/DynamicTypeScriptCompilerMiddleware.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -25,6 +26,10 @@ namespace RockWeb.TypeScript
 
         private readonly ITypeScriptCompiler _compiler;
         private readonly RequestDelegate _next;
+
+        private readonly Dictionary<string, CachedContent> _cachedFiles = new Dictionary<string, CachedContent>();
+
+        private readonly int _cacheTime = 3600;
 
         #endregion
 
@@ -53,56 +58,72 @@ namespace RockWeb.TypeScript
         public async Task InvokeAsync( HttpContext context )
         {
             var path = context.Request.Path.Value;
+            System.IO.FileInfo fileInfo = null;
 
-            if ( path.EndsWith( ".js" ) )
+            if ( !path.EndsWith( ".js" ) && !path.EndsWith( ".js.map" ) )
             {
-                var tsFile = path.Substring( 0, path.Length - 3 ) + ".ts";
-                var tsPath = System.IO.Path.Join( "wwwroot", tsFile );
-                if ( System.IO.File.Exists( tsPath ) )
-                {
-                    var results = _compiler.CompileFile( tsPath );
-                    if ( results.Success )
-                    {
-                        context.Response.ContentType = "application/javascript";
-                        context.Response.ContentLength = results.SourceCode.Length;
-                        await context.Response.WriteAsync( results.SourceCode );
-                    }
-                    else
-                    {
-                        context.Response.StatusCode = 500;
-                        await context.Response.WriteAsync( "Compilation failed." );
-                    }
+                await _next( context );
+                return;
+            }
 
+            string basePath = path.Replace( path.EndsWith( ".js" ) ? ".js" : ".js.map", string.Empty );
+            var tsPath = System.IO.Path.Join( "wwwroot", basePath ) + ".ts";
+
+            fileInfo = new System.IO.FileInfo( tsPath );
+            if ( !fileInfo.Exists )
+            {
+                await _next( context );
+                return;
+            }
+
+            //
+            // See if we have it cached.
+            //
+            if ( _cachedFiles.ContainsKey( path ) )
+            {
+                if ( context.Request.Headers.ContainsKey( "If-None-Match" ) && context.Request.Headers["If-None-Match"] == _cachedFiles[path].ETag )
+                {
+                    context.Response.StatusCode = 304;
                     return;
                 }
             }
 
-            if ( path.EndsWith( ".js.map" ) )
-            {
-                var tsFile = path.Substring( 0, path.Length - 7 ) + ".ts";
-                var tsPath = System.IO.Path.Join( "wwwroot", tsFile );
-                if ( System.IO.File.Exists( tsPath ) )
-                {
-                    var results = _compiler.CompileFile( tsPath );
-                    if ( results.Success )
-                    {
-                        context.Response.ContentType = "application/json";
-                        context.Response.ContentLength = results.SourceMap.Length;
-                        await context.Response.WriteAsync( results.SourceMap );
-                    }
-                    else
-                    {
-                        context.Response.StatusCode = 500;
-                        await context.Response.WriteAsync( "Compilation failed." );
-                    }
+            var results = _compiler.CompileFile( tsPath );
 
-                    return;
-                }
+            //
+            // Store the cached result or inform the user that compilation failed.
+            //
+            if ( results.Success )
+            {
+                _cachedFiles.AddOrReplace( basePath + ".js", new CachedContent { Content = results.SourceCode, ETag = fileInfo.LastWriteTime.ToString() } );
+                _cachedFiles.AddOrReplace( basePath + ".js.map", new CachedContent { Content = results.SourceMap, ETag = fileInfo.LastWriteTime.ToString() } );
+            }
+            else
+            {
+                context.Response.StatusCode = 500;
+                await context.Response.WriteAsync( "Compilation failed." );
             }
 
-            await _next( context );
+            var cachedFile = _cachedFiles[path];
+
+            //
+            // Send the response.
+            //
+            context.Response.ContentType = "application/javascript";
+            context.Response.ContentLength = cachedFile.Content.Length;
+            context.Response.Headers.AddOrReplace( "ETag", cachedFile.ETag );
+            context.Response.Headers.AddOrReplace( "Pragma", "cache" );
+            context.Response.Headers.AddOrReplace( "Cache-Control", $"max-age={_cacheTime}" );
+            await context.Response.WriteAsync( cachedFile.Content );
         }
 
         #endregion
+
+        private class CachedContent
+        {
+            public string Content { get; set; }
+
+            public string ETag { get; set; }
+        }
     }
 }
